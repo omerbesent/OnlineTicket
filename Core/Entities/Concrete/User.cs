00652609e0b88a5701f485ee556bbadbcb04649c
omerbesent <omer.besent@hotmail.com>
2021-09-16 00:38:23 +0300

    user sistemi

---
+++ Core/Entities/Concrete/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Core.Entities.Concrete
+{
+    public class User : IEntity
+    {
+        public int Id { get; set; }
+        public string CitizenId { get; set; }
+        public string FullName { get; set; }
+        public string Email { get; set; }
+        public string MobilePhone { get; set; }
+        public DateTime? BirthDate { get; set; }
+        public byte Gender { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public byte[] PasswordHash { get; set; }
+        public byte[] PasswordSalt { get; set; }
+        public bool Status { get; set; }
+    }
+}
---
+++ Entities/DTOs/UserForRegisterDto.cs
@@ -0,0 +1,12 @@
+﻿using Core;
+
+namespace Entities.DTOs
+{
+    public class UserForRegisterDto : IDto
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public string FullName { get; set; }
+        public string MobilePhone { get; set; }
+    }
+}
